dffc9df4668e7d4e4d6fcc51b5af64eebc425fe9
Trevor Sherrard <sherrardtr@gmail.com>
2023-10-14 18:32:10 -0400

    ran clang-format

--- src/appStateMachine/src/appStateMachine.cpp
+++ src/appStateMachine/src/appStateMachine.cpp
@@ -1,32 +1,35 @@
 #include "../include/appStateMachine.h"
 #include <cstdint>
 
-ApplicationStateMachine::ApplicationStateMachine(){
-    this->current_state = BOUNDARY_SELECTION;
+ApplicationStateMachine::ApplicationStateMachine() {
+  this->current_state = BOUNDARY_SELECTION;
 }
 
-ApplicationStateMachine::ApplicationStateMachine(const ApplicationStateMachine &other){
-    this->current_state = other.current_state;
+ApplicationStateMachine::ApplicationStateMachine(
+    const ApplicationStateMachine &other) {
+  this->current_state = other.current_state;
 }
 
-ApplicationStateMachine ApplicationStateMachine::operator=(const ApplicationStateMachine &other) {
-    this->current_state = other.current_state;
-    return *this;
+ApplicationStateMachine
+ApplicationStateMachine::operator=(const ApplicationStateMachine &other) {
+  this->current_state = other.current_state;
+  return *this;
 }
 
 void ApplicationStateMachine::advanceStateMachine() {
-    // if we aren't at the end of the state machine,
-    // simply increment the state.
-    if(this->getCurrentState() != MAP_GENERATED){
-        this->current_state = static_cast<application_state_t> ((uint8_t) this->getCurrentState() + 1);
-    }
+  // if we aren't at the end of the state machine,
+  // simply increment the state.
+  if (this->getCurrentState() != MAP_GENERATED) {
+    this->current_state =
+        static_cast<application_state_t>((uint8_t)this->getCurrentState() + 1);
+  }
 
-    // otherwise, wrap around to the start.
-    else {
-        this->current_state = BOUNDARY_SELECTION;
-    }
+  // otherwise, wrap around to the start.
+  else {
+    this->current_state = BOUNDARY_SELECTION;
+  }
 }
 
 application_state_t ApplicationStateMachine::getCurrentState() {
-    return this->current_state;
+  return this->current_state;
 }
--- src/appStateMachine/test/test_state_machine.cpp
+++ src/appStateMachine/test/test_state_machine.cpp
@@ -1,31 +1,31 @@
 #include "../include/appStateMachine.h"
 #include "gtest/gtest.h"
 
-TEST(stateMachineTesting, TestStateMachineCreation){
-    // test all constructor permutations
-    ApplicationStateMachine new_state_machine;
-    ApplicationStateMachine copied_state_machine(new_state_machine);
-    ApplicationStateMachine copy_assign_state_machine;
-    copy_assign_state_machine = copied_state_machine;
+TEST(stateMachineTesting, TestStateMachineCreation) {
+  // test all constructor permutations
+  ApplicationStateMachine new_state_machine;
+  ApplicationStateMachine copied_state_machine(new_state_machine);
+  ApplicationStateMachine copy_assign_state_machine;
+  copy_assign_state_machine = copied_state_machine;
 
-    ASSERT_EQ(BOUNDARY_SELECTION, new_state_machine.getCurrentState());
-    ASSERT_EQ(BOUNDARY_SELECTION, copied_state_machine.getCurrentState());
-    ASSERT_EQ(BOUNDARY_SELECTION, copy_assign_state_machine.getCurrentState());
+  ASSERT_EQ(BOUNDARY_SELECTION, new_state_machine.getCurrentState());
+  ASSERT_EQ(BOUNDARY_SELECTION, copied_state_machine.getCurrentState());
+  ASSERT_EQ(BOUNDARY_SELECTION, copy_assign_state_machine.getCurrentState());
 }
 
-TEST(stateMachineTesting, TestStateMachineAdvance){
-    // create state machine
-    ApplicationStateMachine new_state_machine;
+TEST(stateMachineTesting, TestStateMachineAdvance) {
+  // create state machine
+  ApplicationStateMachine new_state_machine;
 
-    // make sure basic state incrementing works
-    for(uint8_t i = 0; i < static_cast<uint8_t>(MAP_GENERATED); i++) {
-        application_state_t cur_state = new_state_machine.getCurrentState();
-        new_state_machine.advanceStateMachine();
-        ASSERT_EQ(static_cast<application_state_t>(i), cur_state);
-    }
-    // make sure we wrap around correctly when we advance from the final state
+  // make sure basic state incrementing works
+  for (uint8_t i = 0; i < static_cast<uint8_t>(MAP_GENERATED); i++) {
+    application_state_t cur_state = new_state_machine.getCurrentState();
     new_state_machine.advanceStateMachine();
-    ASSERT_EQ(BOUNDARY_SELECTION, new_state_machine.getCurrentState());
+    ASSERT_EQ(static_cast<application_state_t>(i), cur_state);
+  }
+  // make sure we wrap around correctly when we advance from the final state
+  new_state_machine.advanceStateMachine();
+  ASSERT_EQ(BOUNDARY_SELECTION, new_state_machine.getCurrentState());
 }
 
 int main(int argc, char **argv) {
