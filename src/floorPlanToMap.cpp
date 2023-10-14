b9c99ff0b91ecfdca29ceeb167dfb05d9f8c3d24
Trevor Sherrard <sherrardtr@gmail.com>
2023-10-14 19:55:34 -0400

    update main file

--- src/floorPlanToMap.cpp
+++ src/floorPlanToMap.cpp
@@ -1,5 +1,23 @@
-#include <opencv2/core.hpp>
+#include "cmdLineArgHandler/include/cmdLineArgHandler.h"
 
-int main() {
+int main(int argc, char **argv) {
+  // process command line arguments
+  CommandLineArgHandler cmd_arg_handler;
+  if (argc < 2) {
+    std::cerr
+        << "You must provide path to a directory of images, or a single image!"
+        << std::endl;
+    cmd_arg_handler.printUsageMenu();
+    return -1;
+  }
+
+  std::map<std::string, cv::Mat> img_map =
+      cmd_arg_handler.parseCommandLineArgs(argv);
+
+  // for each provided image, perform processing procedure
+  for (auto elem : img_map) {
+    // TODO: process images here.
+  }
+
   return 0;
 }
